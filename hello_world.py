71f243a5e66d41431ac98c693e0cff6ddd59debb
Gagansagar108 <79242532+Gagansagar108@users.noreply.github.com>
2021-09-26 10:32:43 +0530

    Add files via upload

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,12 @@
-import cv2
-print("Hello Danish,ratupura wale")
+from flask import Flask,render_template,request
+
+app=Flask(__name__)
+
+@app.route('/',methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+if __name__=='__main__':
+    app.run(debug=True)
+    app.run()
